bcf325d90b5d7b8327499b0e52e8f2a4c5af48d7
Simo Ezzarhouni <simo@Simos-MacBook-Pro.local>
2025-10-25 18:06:12 -0400

    baseline

--- atlas-morocco/playwright.config.ts
+++ atlas-morocco/playwright.config.ts
@@ -17,9 +17,5 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: 'http://localhost:3001',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+ 
 });
