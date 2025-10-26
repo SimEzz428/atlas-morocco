c829838564e856d950484b76443c155831d15bba
Simo Ezzarhouni <simo@Simos-MacBook-Pro.local>
2025-10-26 11:30:35 -0400

    Add test API endpoint

---
+++ atlas-morocco/src/pages/api/test.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ 
+    message: 'API is working',
+    timestamp: new Date().toISOString(),
+    env: {
+      hasDatabaseUrl: !!process.env.DATABASE_URL,
+      hasNextAuthSecret: !!process.env.NEXTAUTH_SECRET,
+    }
+  });
+}
