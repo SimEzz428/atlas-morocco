cedc2b47208c5bf37a8dc2441fc6d8906a4008e1
Simo Ezzarhouni <simo@Simos-MacBook-Pro.local>
2025-10-26 11:35:16 -0400

    Add useSecureCookies to NextAuth config

--- atlas-morocco/src/lib/auth.ts
+++ atlas-morocco/src/lib/auth.ts
@@ -67,6 +67,7 @@ export const authOptions = {
   
   secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === "development",
+  useSecureCookies: process.env.NODE_ENV === "production",
 };
 
 export default NextAuth(authOptions);
